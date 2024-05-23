1852d742ac024e0d10386dc55c4ec38f4b83e035
Tonic-rat <168669223+Tonic-rat@users.noreply.github.com>
2024-05-23 12:37:22 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-const http = require("http");
-const host = 'localhost';
-const port = 8000;
-const fs = require('fs').promises;
-
-const requestListener = function (req, res) {
-    fs.readFile(__dirname + "/Application/Home.html")
-};
